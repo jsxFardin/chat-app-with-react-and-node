b560aef1deeace5966f331ebe234da9d75a04383
fardin.ahsan75@gmail.com <fardin.ahsan75@gmail.com>
2023-11-27 03:09:24 +0600

    file

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -9,6 +9,7 @@ export default function Chat() {
 
     const [ws, setWs] = useState(null);
     const [onlinePeople, setOnlinePeople] = useState({});
+    const [offlinePeople, setOfflinePeople] = useState({});
     const [selectedUserId, setSelectedUserId] = useState(null);
     const { username, id, setId, setUsername } = useContext(UserContext);
     const [newMessageText, setNewMessageText] = useState('');
@@ -46,14 +47,32 @@ export default function Chat() {
         }
     }, [selectedUserId]);
 
+    useEffect(() => {
+        axios.get('/people').then(res => {
+            const offlinePeopleArr = res.data
+                .filter(p => p._id !== id)
+                .filter(p => !Object.keys(onlinePeople).includes(p._id));
+            const offlinePeople = {};
+            offlinePeopleArr.forEach(p => {
+                offlinePeople[p._id] = p;
+            });
+            setOfflinePeople(offlinePeople);
+        });
+    }, [onlinePeople]);
+
+    function logout() {
+        axios.post('/logout').then(() => {
+            setWs(null);
+            setId(null);
+            setUsername(null);
+        });
+    }
 
     function showOnlinePeople(peopleArray) {
         const people = {};
         peopleArray.forEach(({ userId, username }) => {
             people[userId] = username;
         });
-        // console.log(people);
-
         setOnlinePeople(people);
     }
     function handleMessage(ev) {
@@ -62,25 +81,41 @@ export default function Chat() {
             showOnlinePeople(messageData.online);
         }
         else if ('text' in messageData) {
-            // if (messageData.sender === selectedUserId) {
-            setMessages(prev => ([...prev, { ...messageData }]));
-            // }
+            if (messageData.sender === selectedUserId) {
+                setMessages(prev => ([...prev, { ...messageData }]));
+            }
         }
     }
-
-    function sendMessage(ev) {
-        ev.preventDefault();
+    function sendFile(ev) {
+        const reader = new FileReader();
+        reader.readAsDataURL(ev.target.files[0]);
+        reader.onload = () => {
+            sendMessage(null, {
+                name: ev.target.files[0].name,
+                data: reader.result,
+            });
+        };
+    }
+    function sendMessage(ev, file = null) {
+        if (ev) ev.preventDefault();
         ws.send(JSON.stringify({
             recipient: selectedUserId,
             text: newMessageText,
+            file,
         }));
-        setNewMessageText('');
-        setMessages(prev => ([...prev, {
-            text: newMessageText,
-            sender: id,
-            recipient: selectedUserId,
-            _id: Date.now(),
-        }]));
+        if (file) {
+            axios.get('/messages/' + selectedUserId).then(res => {
+                setMessages(res.data);
+            });
+        } else {
+            setNewMessageText('');
+            setMessages(prev => ([...prev, {
+                text: newMessageText,
+                sender: id,
+                recipient: selectedUserId,
+                _id: Date.now(),
+            }]));
+        }
     }
     const onlinePeopleExclOurUser = { ...onlinePeople };
     delete onlinePeopleExclOurUser[id];
@@ -99,9 +134,29 @@ export default function Chat() {
                             id={userId}
                             online={true}
                             username={onlinePeopleExclOurUser[userId]}
-                            onClick={() => { setSelectedUserId(userId); console.log({ userId }) }}
+                            onClick={() => { setSelectedUserId(userId) }}
                             selected={userId === selectedUserId} />
                     ))}
+                    {Object.keys(offlinePeople).map(userId => (
+                        <Contact
+                            key={userId}
+                            id={userId}
+                            online={false}
+                            username={offlinePeople[userId].username}
+                            onClick={() => setSelectedUserId(userId)}
+                            selected={userId === selectedUserId} />
+                    ))}
+                </div>
+                <div className="p-2 text-center flex items-center justify-center">
+                    <span className="mr-2 text-sm text-gray-600 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+                        </svg>
+                        {username}
+                    </span>
+                    <button
+                        onClick={logout}
+                        className="text-sm bg-blue-100 py-1 px-2 text-gray-500 border rounded-sm">logout</button>
                 </div>
             </div>
             <div className="flex flex-col bg-blue-50 w-2/3 p-2">
@@ -118,6 +173,18 @@ export default function Chat() {
                                     <div key={message._id} className={"mr-2 " + (message.sender === id ? 'text-right' : 'text-left')}>
                                         <div className={"text-left inline-block p-2 my-2 rounded-md text-sm " + (message.sender === id ? 'bg-blue-500 text-white' : 'bg-white text-gray-500')}>
                                             {message.text}
+                                            {message.file && (
+                                                <div className="">
+                                                    <a target="_blank"
+                                                        className="flex items-center gap-1 border-b"
+                                                        href={axios.defaults.baseURL + '/uploads/' + message.file}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                                            <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
+                                                        </svg>
+                                                        {message.file}
+                                                    </a>
+                                                </div>
+                                            )}
                                         </div>
                                     </div>
                                 ))}
@@ -134,7 +201,7 @@ export default function Chat() {
                             placeholder="Type your message here"
                             className="bg-white flex-grow border rounded-sm p-2" />
                         <label className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
-                            <input type="file" className="hidden" />
+                            <input type="file" className="hidden" onChange={sendFile} />
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                 <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
                             </svg>
